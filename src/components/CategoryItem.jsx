160388304c0f77b0ccc2f66aef72cbccdba3f112
Ivan Shchururpov <vanya.schurupov@yandex.ru>
2023-09-17 12:00:27 +0300

    change position headers

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -4,13 +4,12 @@ function CategoryItem(props) {
   const { strCategory, strCategoryThumb, strCategoryDescription } = props;
   return (
     <div className="card">
-      <div className="card-content card-content-title">
-        <span className="card-title">{strCategory}</span>
-      </div>
       <div className="card-image">
         <img src={strCategoryThumb} alt={strCategory} />
       </div>
       <div className="card-content">
+        <span className="card-title">{strCategory}</span>
+
         <p>{strCategoryDescription.slice(0, 60)} ...</p>
       </div>
       <div className="card-action">
